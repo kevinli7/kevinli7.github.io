8edcc999170119027c5a6015901d60225a81e6d7
kevin <kevinli7@gmail.com>
2015-02-20 01:57:55 -0800

    testing

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 var main = function() {
   /* Push the body and the nav over by 285px over */
   $('.icon-menu').click(function() {
-    var top = document.defaultView.getComputedStyle(menu, null).getPropertyValue("top");
-      if (top != 0){
-        $('.menu').animate({
-          top: "0px"
-        }, 100);
-      } else {
-        $('.menu').animate({
-          top: "-66px"
-        }, 100);
-      }
+    var elem = document.getElementById(".menu");
+    var top = document.defaultView.getComputedStyle(elem, null).getPropertyValue("top");
+    console.log(top);
+    if (top != 0){
+      $('.menu').animate({
+        top: "0px"
+      }, 100);
+    } else {
+      $('.menu').animate({
+        top: "-66px"
+      }, 100);
+    }
   });
 };
 

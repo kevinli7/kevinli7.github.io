c78f9824d284710428ef37bd965c7abf80edcaca
kevin <kevinli7@gmail.com>
2015-02-20 01:55:28 -0800

    changed js

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 var main = function() {
   /* Push the body and the nav over by 285px over */
   $('.icon-menu').click(function() {
-    var tog = 1;
-      if (tog == 1){
-        tog = 0;
+    var top = document.defaultView.getComputedStyle(menu, null).getPropertyValue("top");
+      if (top != 0){
         $('.menu').animate({
           top: "0px"
         }, 100);
-        // $('body').animate({
-        //   left: "285px"
-        // }, 200);
       } else {
-        tog = 1;
         $('.menu').animate({
           top: "-66px"
         }, 100);

15c8ae179be6375f91963010ec436448cf861502
kevin <kevinli7@gmail.com>
2015-02-20 02:04:33 -0800

    test

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var main = function() {
   /* Push the body and the nav over by 285px over */
   $('.icon-menu').click(function() {
     var top = $( this ).css( "top" );
+    console.log(top);
     if (top != 0){
       $('.menu').animate({
         top: "0px"

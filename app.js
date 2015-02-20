b7cfdd9028f05b3cdb92a3399b0ce173eb0aa3db
kevin <kevinli7@gmail.com>
2015-02-20 02:14:33 -0800

    test

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 var main = function() {
   /* Push the body and the nav over by 285px over */
-  $('.icon-menu').click(function() {
-    var top = $('.menu').css( "top" );
-    console.log(top);
-    if (top != '0px'){
-      $('.menu').animate({
+  $('.icon-menu').hover(function() {
+     $('.menu').animate({
         top: "0px"
-      }, 100);
-    } else {
+      }, 100);}, function() {
       $('.menu').animate({
         top: "-66px"
       }, 100);
-    }
-  });
+     }
+  )};
 };
+
+//   $('.icon-menu').click(function() {
+//     var top = $('.menu').css( "top" );
+//     console.log(top);
+//     if (top != '0px'){
+//       $('.menu').animate({
+//         top: "0px"
+//       }, 100);
+//     } else {
+//       $('.menu').animate({
+//         top: "-66px"
+//       }, 100);
+//     }
+//   });
+// };
 
 $(document).ready(main);

e4c1e0ce9e2784f5a8eb96185adb33134fabaf3c
kevin <kevinli7@gmail.com>
2015-02-20 02:39:07 -0800

    test

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var main = function() {
   /* Push the body and the nav over by 285px over */
-  $('icon-menu').hover(
+  $('.icon-menu').hover(
       function() {$('.menu').animate({top: "0px"}, 100);}, 
       function() {$('.menu').animate({top: "-66px"}, 100);}
   )
